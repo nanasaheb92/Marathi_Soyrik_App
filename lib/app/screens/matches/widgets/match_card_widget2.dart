b2851bd780eaf8dda5635d18ec36d8dea45cdefe
Nanasaheb Rupnar <nanasaheb.rupnar@appetals.com>
2026-09-12 09:25:42 +0530

    first changes after share button add and role base

--- lib/app/screens/matches/widgets/match_card_widget2.dart
+++ lib/app/screens/matches/widgets/match_card_widget2.dart
@@ -217,16 +217,16 @@ class _MatchCardWidgetState extends State<MatchCardWidget> {
 🔸सध्याचा पत्ता :* ${getValue(fullProfile?.location ?? widget.profile.location)}
 🔸अपेक्षा :* ${getValue(fullProfile?.partnerPreferance?.generalExpt)}
 
-अधिक माहितीसाठी खालील लिंक वर क्लिक करावे.👇       
+*अधिक माहितीसाठी खालील लिंक वर क्लिक करावे.👇*       
 https://www.marathisoyrik.in/viewFullProfile.php?id=$profileId
 
 🌺🌺🌺🌺🌺🌺
 
-संपर्क : मराठा सोयरीक संस्था,
+*संपर्क : मराठा सोयरीक संस्था,*
 महाराष्ट्रातील नं. १ विश्वसनीय वधुवर सुचक संस्था 
-आमच्या शाखा  
+*आमच्या शाखा*  
 $bLoc $bMob
-वेळ : स.10 ते सायं.7 पर्यंत
+*वेळ : स.10 ते सायं.7 पर्यंत*
 👩‍❤️‍👨   👩‍❤️‍👨   👩‍❤️‍👨  👩‍❤️‍👨  👩‍❤️‍👨
 ''';
 
